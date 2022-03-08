d49b43f5010f9821d1d1c89c03c41fa0dbbc6cf1
BAK BASS JUNIOR <BAK BASS JUNIOR@DESKTOP-GTR0TMO>
2022-03-08 04:20:16 +0100

    Creation page enregistrer parent et etudiant Modifier Quelques page

--- GESTION DE CONCIERGERIE/src/application/Main.java
+++ GESTION DE CONCIERGERIE/src/application/Main.java
@@ -5,16 +5,16 @@ import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			//BorderPane root = new BorderPane();
-			Parent root = new FXMLLoader().load(getClass().getResource("Acceuil.fxml"));
+			
+			Parent root = FXMLLoader.load(getClass().getResource("Acceuil.fxml"));
 			Scene scene = new Scene(root,400,400);
+			
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
