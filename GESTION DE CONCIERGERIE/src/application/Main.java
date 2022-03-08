9a7da90626372c835c53d6f9be7d79cf095cd82f
BAK BASS JUNIOR <BAK BASS JUNIOR@DESKTOP-GTR0TMO>
2022-03-09 00:00:27 +0100

    ????

--- GESTION DE CONCIERGERIE/src/application/Main.java
+++ GESTION DE CONCIERGERIE/src/application/Main.java
@@ -17,7 +17,7 @@ public class Main extends Application {
 			
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			
-			primaryStage.setTitle("Hello World");
+			primaryStage.setTitle("Hello ");
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
