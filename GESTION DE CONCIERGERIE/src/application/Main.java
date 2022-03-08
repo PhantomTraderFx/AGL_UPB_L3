8c40b747e21e6be72ff39210f511252bb57caf5e
LENOVO <LENOVO@DJABIA-ANGE>
2022-03-08 11:57:43 +0000

    rien d'important

--- GESTION DE CONCIERGERIE/src/application/Main.java
+++ GESTION DE CONCIERGERIE/src/application/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			
-			Parent root = FXMLLoader.load(getClass().getResource("Acceuil.fxml"));
+			Parent root = FXMLLoader.load(getClass().getResource("commande.fxml"));
 			Scene scene = new Scene(root,400,400);
 			
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
